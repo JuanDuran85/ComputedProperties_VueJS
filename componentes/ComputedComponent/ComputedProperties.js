48f7333cba6f6c60d02c7d39567d11ed128e4b2b
JuanDuran85 <duran85juan@gmail.com>
2020-01-05 14:49:11 -0300

    agregando valores al computed

--- componentes/ComputedComponent/ComputedProperties.js
+++ componentes/ComputedComponent/ComputedProperties.js
@@ -2,11 +2,22 @@ Vue.component('computed-properties', {
     template: `
         <div>
             <h1>Computed Properties desde Componente</h1>
+            <p>Llamado directo sin computed: {{usuario.nombre}} {{usuario.apellido}}</p>
+            <p>Llamado directo con computed: {{fullName}}</p>
         </div>
     `,
     data() {
         return {
-            
+            usuario: {
+                nombre: 'Juan',
+                apellido: 'Duran',
+                year: '1985'
+            }
+        }
+    },
+    computed: {
+        fullName(){
+            return `${this.usuario.nombre} ${this.usuario.apellido}`
         }
     },
 })
