ea5e7280658e691a566730b75bd5a74caa5b568f
JuanDuran85 <duran85juan@gmail.com>
2020-01-05 15:12:38 -0300

    interactuando con computed y methods

--- componentes/ComputedComponent/ComputedProperties.js
+++ componentes/ComputedComponent/ComputedProperties.js
@@ -4,6 +4,8 @@ Vue.component('computed-properties', {
             <h1>Computed Properties desde Componente</h1>
             <p>Llamado directo sin computed: {{usuario.nombre}} {{usuario.apellido}}</p>
             <p>Llamado directo con computed: {{fullName}}</p>
+            <p>Llamado con methods: {{nombreCompleto()}} {{edadUsuario}}</p>
+            <p>La edad del usuario es: {{edadUsuario}} años</p>
         </div>
     `,
     data() {
@@ -18,6 +20,21 @@ Vue.component('computed-properties', {
     computed: {
         fullName(){
             return `${this.usuario.nombre} ${this.usuario.apellido}`
+        },
+        edadUsuario(){
+            let fecha = new Date().getFullYear();
+            let cumple = fecha - this.usuario.year;
+            return cumple
+        }
+    },
+    methods: {
+        nombreCompleto(){
+            return `${this.usuario.nombre} ${this.usuario.apellido}`
         }
     },
 })
+
+// las computed guardan la inforamcion en cache y espera hasta que el elemento se actualice
+// para volver a renderizar
+
+//los method consumen mas recursos porque son un proceso mas complejo
